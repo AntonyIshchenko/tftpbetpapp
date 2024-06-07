67e957596f50bcf3e2b83e01c27b6746208eab74
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-06-07 23:00:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRouter);
-app.use('/api/tasks', authMiddleware, taskRouter);
+app.use('/api', authMiddleware, taskRouter);
 
 // app.get('/api/test', async (req, res, next) => {
 //   try {

a3384005b01cae65cf16babc95ab91291b7a7cfd
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-06-08 15:32:49 +0200

    Update columnModel.js

--- schemas/columnModel.js
+++ schemas/columnModel.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const columnSchema = new mongoose.Schema({
-    //назва колонки
     name: {
         type: String,
         required: [true, 'Set name for Column'],
@@ -13,12 +12,8 @@ const columnSchema = new mongoose.Schema({
         ref: 'Board', // посилаємось на модель дошки 'Board'
     },
 },
-    //створення та оновлення колонки
     { timestamps: true, versionKey: false } // залишаємо ?
 );
 
 const Column = mongoose.model('Column', columnSchema);
 export default Column;
-
-/* export default mongoose.model('Column', columnSchema); 
-*/

3dda39b598471af7d24ad01d7cf29e126ab02c5a
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-06-08 15:32:43 +0200

    Update taskRouter.js

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -3,21 +3,21 @@ import taskCtrls from '../controllers/taskCtrl.js';
 
 const taskRouter = express.Router();
 
-// Шляхи до дошки (повернути всі, певну дошку, створити, змінити, видалити)
+// Шляхи до Board
 taskRouter.get('/boards', taskCtrls.getAllBoards);
 taskRouter.get('/boards/:id', taskCtrls.getOneBoard);
 taskRouter.post('/boards', taskCtrls.createBoard);
 taskRouter.patch('/boards/:id', taskCtrls.editBoard);
 taskRouter.delete('/boards/:id', taskCtrls.deleteBoard);
 
-//Шляхи до колонок(створити, змінити, видалити)
-taskRouter.post('/columns', taskCtrls.createColumn);
+//Шляхи до Column
+taskRouter.post('/columns/:boardId', taskCtrls.createColumn);
 taskRouter.patch('/columns/:id', taskCtrls.editColumn);
 taskRouter.delete('/columns/:id', taskCtrls.deleteColumn);
 
-//Шляхи до завдань(створити, змінити, видалити)
-taskRouter.post('/tasks', taskCtrls.createTask);
-taskRouter.patch('/tasks/:id', taskCtrls.editTask);
-taskRouter.delete('/tasks/:id', taskCtrls.deleteTask);
+//Шляхи до Task
+taskRouter.post('/tasks/:boardId/:columnId', taskCtrls.createTask);
+taskRouter.patch('/tasks/:boardId/:columnId/:id', taskCtrls.editTask);
+taskRouter.delete('/tasks/:boardId/:columnId/:id', taskCtrls.deleteTask);
 
 export default taskRouter;

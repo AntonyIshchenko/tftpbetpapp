2012650b0872251c07c4c4b8c3342ad94c67f390
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-06-08 15:32:46 +0200

    Update boardModel.js

--- schemas/boardModel.js
+++ schemas/boardModel.js
@@ -1,23 +1,22 @@
 import mongoose from "mongoose";
 
 const boardSchema = new mongoose.Schema({
-    //назва дошки
     name: {
         type: String,
         default: null,
         required: [true, 'Set name for Board'],
     },
-    //власник дошки
+
     owner: {
         type: mongoose.Types.ObjectId,
         ref: 'User', //посилання на модель користувача 'User'
     },
-    // створюємо іконку 
+
     icon: {
         type: String,
         required: [true, 'Set icon for board'],
     },
-    // створюємо бекграунд з типом та посиланням
+
     background: {
         type: {
             type: String,
@@ -29,13 +28,9 @@ const boardSchema = new mongoose.Schema({
         },
     },
 },
-    //створення та оновлення колонки
-    { timestamps: true, versionKey: false } // залишаємо ? 
+    { timestamps: true, versionKey: false }
 );
 
 const Board = mongoose.model('Board', boardSchema);
 export default Board;
-
-/* export default mongoose.model('Board', boardSchema); 
-*/
 

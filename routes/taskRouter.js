7446550870e067a9e52b7f2289d229c8fd2caff2
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-06-10 11:59:38 +0200

    Update taskRouter.js

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,23 +1,48 @@
 import express from 'express';
 import taskCtrls from '../controllers/taskCtrl.js';
+import {
+    createBoardSchema,
+    updateBoardSchema,
+    createColumnSchema,
+    updateColumnSchema,
+    createTaskSchema,
+    updateTaskSchema
+} from '../schemas/tasksSchemas.js';
+import validateBody from '../helpers/validateBody.js';
 
 const taskRouter = express.Router();
 
 // Шляхи до Board
 taskRouter.get('/boards', taskCtrls.getAllBoards);
 taskRouter.get('/boards/:id', taskCtrls.getOneBoard);
-taskRouter.post('/boards', taskCtrls.createBoard);
-taskRouter.patch('/boards/:id', taskCtrls.editBoard);
+taskRouter.post('/boards',
+    validateBody(createBoardSchema),
+    taskCtrls.createBoard);
+taskRouter.patch('/boards/:id',
+    validateBody(updateBoardSchema),
+    taskCtrls.editBoard);
 taskRouter.delete('/boards/:id', taskCtrls.deleteBoard);
 
 //Шляхи до Column
-taskRouter.post('/columns/', taskCtrls.createColumn);
-taskRouter.patch('/columns/:id', taskCtrls.editColumn);
+taskRouter.post(
+    '/columns/',
+    validateBody(createColumnSchema),
+    taskCtrls.createColumn);
+taskRouter.patch(
+    '/columns/:id',
+    validateBody(updateColumnSchema),
+    taskCtrls.editColumn);
 taskRouter.delete('/columns/:id', taskCtrls.deleteColumn);
 
 //Шляхи до Task
-taskRouter.post('/tasks/', taskCtrls.createTask);
-taskRouter.patch('/tasks/:id', taskCtrls.editTask);
+taskRouter.post(
+    '/tasks/',
+    validateBody(createTaskSchema),
+    taskCtrls.createTask);
+taskRouter.patch(
+    '/tasks/:id',
+    validateBody(updateTaskSchema),
+    taskCtrls.editTask);
 taskRouter.delete('/tasks/:id', taskCtrls.deleteTask);
 
 export default taskRouter;

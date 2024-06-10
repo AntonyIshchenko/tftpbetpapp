46ab8fb9d61a21e3979e89d2a634c4d717b500a5
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-06-10 21:40:49 +0200

    Update tasksSchemas.js

--- schemas/tasksSchemas.js
+++ schemas/tasksSchemas.js
@@ -1,85 +1,79 @@
 import Joi from "joi";
+import mongoose from "mongoose";
+
+
+const validateObjectId = (fieldName, error) => {
+    return Joi.string().required().messages({
+        'any.required': `Field ${fieldName} is required`
+    }).custom((id, obj) => {
+        if (!mongoose.isValidObjectId(id)) {
+            return obj.message(error);
+        }
+        return obj;
+    });
+};
+
+const deadlineRegex = /^\d{4}-\d{2}-\d{2}$/;
 
 // ------------ Board Joi Schema
 export const createBoardSchema = Joi.object({
-    name: Joi.string().min(2).max(32).required().messages({
-        'any.required': 'Field "name" is required',
-        'string.empty': 'Field "name" cannot be empty',
-        'string.min': 'Field "name" must be at least 2 characters long',
-        'string.max': 'Field "name" must be at most 32 characters long',
+    name: Joi.string().required().messages({
+        'any.required': 'Field name is required',
+        'string.empty': 'Field name cannot be empty',
     }),
-    owner: Joi.string().hex().length(24).messages({
-        'string.hex': 'Invalid owner ID format',
-        'string.lenght': 'Invalid owner ID lenght'
+    owner: Joi.string().custom((id, obj) => {
+        if (!mongoose.isValidObjectId(id)) {
+            return obj.message('Invalid owner ID format');
+        }
+        return obj;
     }),
     icon: Joi.string().required().messages({
-        'any.required': 'Set icon for board',
-        'string.empty': 'Field "icon" cannot be empty'
+        'any.required': 'Set icon for Board',
+        'string.empty': 'Field icon cannot be empty'
     }),
-    background: Joi.object({
-        type: Joi.string().optional(),
-        url: Joi.string().optional()
-    })
+    background: Joi.string().optional()
 });
 
 export const updateBoardSchema = Joi.object({
-    name: Joi.string().min(2).max(32),
+    name: Joi.string(),
     icon: Joi.string(),
-    background: Joi.object({
-        type: Joi.string().optional(),
-        url: Joi.string().optional()
-    })
+    background: Joi.string().optional()
 });
 
 // ------------ Column Joi Schema
 export const createColumnSchema = Joi.object({
-    name: Joi.string().min(2).max(32).required().messages({
-        'any.required': 'Field "name" is required',
-        'string.empty': 'Field "name" cannot be empty',
-        'string.min': 'Field "name" must be at least 2 characters long',
-        'string.max': 'Field "name" must be at most 32 characters long',
+    name: Joi.string().required().messages({
+        'any.required': 'Field name is required',
+        'string.empty': 'Field name cannot be empty',
     }),
-    boardId: Joi.string().hex().length(24).required().messages({
-        'any.required': 'Field "boardId" is required',
-        'string.hex': 'Invalid owner ID format',
-        'string.lenght': 'Invalid owner ID lenght'
-    })
+    boardId: validateObjectId('board ID', 'Invalid board ID format')
 });
 
 export const updateColumnSchema = Joi.object({
-    name: Joi.string().min(2).max(32),
+    name: Joi.string(),
 });
 
 // ------------ Task Joi Schema
 
 export const createTaskSchema = Joi.object({
-    name: Joi.string().min(2).max(32).required().messages({
-        'any.required': 'Field "name" is required',
-        'string.empty': 'Field "name" cannot be empty',
-        'string.min': 'Field "name" must be at least 2 characters long',
-        'string.max': 'Field "name" must be at most 32 characters long',
+    name: Joi.string().required().messages({
+        'any.required': 'Field name is required',
+        'string.empty': 'Field name cannot be empty',
     }),
-    boardId: Joi.string().hex().length(24).required().messages({
-        'any.required': 'Field "boardId" is required',
-        'string.hex': 'Invalid owner ID format',
-        'string.length': 'Invalid owner ID length'
-    }),
-    columnId: Joi.string().hex().length(24).required().messages({
-        'any.required': 'Field "columnId" is required',
-        'string.hex': 'Invalid owner ID format',
-        'string.length': 'Invalid owner ID length'
-    }),
-    description: Joi.string().min(2).max(32).optional(),
+    boardId: validateObjectId('board ID', 'Invalid board ID format'),
+    columnId: validateObjectId('column ID', 'Invalid column ID format'),
+    description: Joi.string().optional(),
     priority: Joi.string().valid('without', 'low', 'medium', 'high').messages({
         'any.only': 'Priority must be one of "without", "low", "medium", "high"'
     }),
-    deadline: Joi.string().allow('').optional(),
+    deadline: Joi.string().allow('').optional().pattern(deadlineRegex)
 });
 
 export const updateTaskSchema = Joi.object({
-    name: Joi.string().min(2).max(32),
-    description: Joi.string().min(2).max(32),
+    name: Joi.string(),
+    description: Joi.string(),
     priority: Joi.string().valid('without', 'low', 'medium', 'high'),
+    deadline: Joi.string().allow('').optional().pattern(deadlineRegex)
 });
 
 

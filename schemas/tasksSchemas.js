68d2314db61e879c33dcdc389f3bb2c1ea9848f3
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-06-12 01:07:34 +0200

    Update tasksSchemas.js

--- schemas/tasksSchemas.js
+++ schemas/tasksSchemas.js
@@ -1,10 +1,10 @@
 import Joi from "joi";
 import mongoose from "mongoose";
 
-
 const validateObjectId = (fieldName, error) => {
-    return Joi.string().required().messages({
-        'any.required': `Field ${fieldName} is required`
+    return Joi.string().required().trim().messages({
+        'any.required': `Field ${fieldName} is required`,
+        'string.empty': `Field ${fieldName} cannot be empty`,
     }).custom((id, obj) => {
         if (!mongoose.isValidObjectId(id)) {
             return obj.message(error);
@@ -13,36 +13,54 @@ const validateObjectId = (fieldName, error) => {
     });
 };
 
-const deadlineRegex = /^\d{4}-\d{2}-\d{2}$/;
+const validateDate = (fieldDate) => {
+    return Joi.string().required().messages({
+        'any.required': `Field ${fieldDate} is required`,
+        'string.empty': `Field ${fieldDate} cannot be empty`,
+    }).custom((date, checker) => {
+        const deadlineRegex = /^\d{4}-\d{2}-\d{2}$/;
+        if (!deadlineRegex.test(date)) {
+            return checker.message(`Date must be in the format YYYY-MM-DD`);
+        }
+
+        const [month, day] = date.split('-').map(Number);
+
+        if (month < 1 || month > 12) {
+            return checker.message(`Month must be between 1 and 12`);
+        }
+
+        if (day < 1 || day > 31) {
+            return checker.message(`Day must be between 1 and 31`)
+        }
+
+        return date;
+    });
+};
+
 
 // ------------ Board Joi Schema
 export const createBoardSchema = Joi.object({
-    name: Joi.string().required().messages({
+    name: Joi.string().trim().required().messages({
         'any.required': 'Field name is required',
         'string.empty': 'Field name cannot be empty',
     }),
-    owner: Joi.string().custom((id, obj) => {
-        if (!mongoose.isValidObjectId(id)) {
-            return obj.message('Invalid owner ID format');
-        }
-        return obj;
-    }),
+    owner: validateObjectId('owner ID', 'Invalid owner ID format'),
     icon: Joi.string().required().messages({
         'any.required': 'Set icon for Board',
         'string.empty': 'Field icon cannot be empty'
     }),
-    background: Joi.string().optional()
+    background: Joi.string().allow('')
 });
 
 export const updateBoardSchema = Joi.object({
-    name: Joi.string(),
+    name: Joi.string().trim(),
     icon: Joi.string(),
-    background: Joi.string().optional()
+    background: Joi.string().allow('')
 });
 
 // ------------ Column Joi Schema
 export const createColumnSchema = Joi.object({
-    name: Joi.string().required().messages({
+    name: Joi.string().trim().required().messages({
         'any.required': 'Field name is required',
         'string.empty': 'Field name cannot be empty',
     }),
@@ -50,32 +68,30 @@ export const createColumnSchema = Joi.object({
 });
 
 export const updateColumnSchema = Joi.object({
-    name: Joi.string(),
+    name: Joi.string().trim().required()
 });
 
 // ------------ Task Joi Schema
 
 export const createTaskSchema = Joi.object({
-    name: Joi.string().required().messages({
+    name: Joi.string().trim().required().messages({
         'any.required': 'Field name is required',
         'string.empty': 'Field name cannot be empty',
     }),
     boardId: validateObjectId('board ID', 'Invalid board ID format'),
     columnId: validateObjectId('column ID', 'Invalid column ID format'),
-    description: Joi.string().optional(),
+    description: Joi.string().trim().allow(''),
     priority: Joi.string().valid('without', 'low', 'medium', 'high').messages({
         'any.only': 'Priority must be one of "without", "low", "medium", "high"'
     }),
-    deadline: Joi.string().allow('').required().pattern(deadlineRegex).messages({
-        'string.pattern.base': 'Deadline must be in the format YYYY- MM - DD, e.g., "2024-12-31"',
-    })
+    deadline: validateDate('deadline', 'Invalid date format or values out of range')
 });
 
 export const updateTaskSchema = Joi.object({
-    name: Joi.string(),
-    description: Joi.string(),
+    name: Joi.string().trim(),
+    description: Joi.string().trim().allow(''),
     priority: Joi.string().valid('without', 'low', 'medium', 'high'),
-    deadline: Joi.string().allow('').required().pattern(deadlineRegex)
+    deadline: validateDate('deadline', 'Invalid date format or values out of range')
 });
 
 

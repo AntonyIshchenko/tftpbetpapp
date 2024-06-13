63d847b70472d9c6d7fff615db0b144e6f168e22
Sinfinka <inadaradar@gmail.com>
2024-06-14 01:02:55 +0300

    del auth from refresh rout

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import authCtrl from '../controllers/authCtrl.js';
-import authMiddleware from '../middlewares/authenticate.js';
 
 const authRouter = express.Router();
 
-authRouter.get('/refresh-token', authMiddleware, authCtrl.refreshToken); // прописати!
+authRouter.get('/refresh-token', authCtrl.refreshToken); // прописати!
 
 export default authRouter;
